f5a87f40d7c8d33bf973cce66e51a0b095bb8d09
Krzysztof Nowicki <krzysiek.nowicki@gmail.com>
2014-09-10 10:24:14 +0200

    video chat project added

---
+++ video-chat/Gruntfile.js
@@ -0,0 +1,38 @@
+module.exports = function(grunt) {
+
+  require('grunt-library-builder');
+
+  grunt.initConfig({
+
+    pkg : grunt.file.readJSON('package.json'),
+
+    bower_concat: {
+	all: {
+	dest: 'dist/video-chat-bower.js'
+	}
+    },
+    concat: {
+      options: {
+        banner: ''
+      },
+      target : {
+        src : ['dist/video-chat-bower.js','src/lib/peer.min.js', 'src/peer-video-chat.js'],
+        dest : 'dist/video-chat.js'
+      }
+    },
+    uglify: {
+          my_target: {
+              files: {
+                  'dist/video-chat.min.js': 'dist/video-chat.js'
+              }
+          }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-bower-concat');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  grunt.registerTask('default', ['bower_concat', 'concat', 'uglify']);
+
+};
