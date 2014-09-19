615cd0286d78e4ed12611d6f88f6f167f6c6deec
swojtowi <Szymon.Wojtowicz@impaqgroup.com>
2014-09-19 12:17:17 +0200

    WordPress cookbook (from community) and its dependencies added

--- chef-repo/site-cookbooks/cms/recipes/install_wordpress.rb
+++ chef-repo/site-cookbooks/cms/recipes/install_wordpress.rb
@@ -1,3 +1,4 @@
 execute "install_wordpress_message" do
-	command "echo 'Install CMS recipe - hello world!'; touch '/tmp/install_cms'"
+	# command "echo 'Install CMS recipe - hello world!'; touch '/tmp/install_cms'"
+	command "echo 'Install CMS recipe - hello world!' > /tmp/stdout.txt 2> /tmp/stderr.txt"
 end
