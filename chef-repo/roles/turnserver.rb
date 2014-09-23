c233b6532dfda16a8fb384bea0559e5f0f81fa3d
davidsie <maxidavid@interia.pl>
2014-09-23 12:33:29 +0200

    minor changes in role: turnserver

--- chef-repo/roles/turnserver.rb
+++ chef-repo/roles/turnserver.rb
@@ -1,6 +1,7 @@
 name "turnserver"
 description "The role installing and configuring TURNserver"
-run_list "recipe[turn::install_turn]", "recipe[turn::configure_turn]"
+run_list "recipe[turnserver]"
+#run_list "recipe[turn::install_turn]", "recipe[turn::configure_turn]"
 # env_run_lists "prod" => ["recipe[apache2]"], "staging" => ["recipe[apache2::staging]"], "_default" => []
 # default_attributes "apache2" => { "listen_ports" => [ "80", "443" ] }
 # override_attributes "apache2" => { "max_children" => "50" }
