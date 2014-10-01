b0f73004b05751abf2f3b9eb9d9ce105f81f43cd
davidsie <maxidavid@interia.pl>
2014-10-01 10:59:50 +0200

    changes in iptables-manager (including name :P )

--- chef-repo/roles/iptables_manager.rb
+++ chef-repo/roles/iptables_manager.rb
@@ -1,6 +1,6 @@
-name "iptables_menager"
+name "iptables_manager"
 description "The role installing and configuring PeerJSserver"
-run_list "recipe[simple_iptables]","recipe[iptables_menager]" # 
+run_list "recipe[simple_iptables]","recipe[iptables_manager]" # 
 #env_run_lists "prod" => ["recipe[apache2]"], "staging" => ["recipe[apache2::staging]"], "_default" => []
 # default_attributes "apache2" => { "listen_ports" => [ "80", "443" ] }
 # override_attributes "apache2" => { "max_children" => "50" }
--- chef-repo/site-cookbooks/iptables_manager/metadata.rb
+++ chef-repo/site-cookbooks/iptables_manager/metadata.rb
@@ -1,8 +1,8 @@
-name             'iptables_menager'
+name             'iptables_manager'
 maintainer       'David Sieciński'
 maintainer_email 'david.siecinski@impaqgroup.com'
 license          'All rights reserved'
-description      'Installs/Configures iptables_menager'
+description      'Installs/Configures iptables_manager'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.0.0'
 
